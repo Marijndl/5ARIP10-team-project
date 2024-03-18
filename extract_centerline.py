848960e88379f96f18de2315273d05f5721ac049
Marijn <51094560+Marijndl@users.noreply.github.com>
2024-03-18 10:38:56 +0100

    Added extract centerline function

--- extract_centerline.py
+++ extract_centerline.py
@@ -1,15 +1,51 @@
 import ExtractCenterline
 extractLogic = ExtractCenterline.ExtractCenterlineLogic()
 
-def extract_centerlines_slicer(label_id: int):
-  # Load the CTA data
-  slicer.util.loadSegmentation(f"c:/Users/20203226/Documents/CTA data/1-200/{label_id}.label.nii.gz")
+def resample_segment(segment, sample_number = 350):
+  # Resample each segment
+  currentPoints = segment.GetCurvePointsWorld()
+  newPoints = vtk.vtkPoints()
+  sampleDist = segment.GetCurveLengthWorld() / (sample_number - 1)
+  closedCurveOption = 0
+  segment.ResamplePoints(currentPoints, newPoints, sampleDist, closedCurveOption)
 
-  # Select the segmentation node
-  segmentationName = f'{label_id}.label.nii.gz' # replace with the name of your segmentation
-  segmentName = 'Segment_1' # replace with the name of the segment you want to find the centerline of
-  segmentationNode = slicer.util.getNode(segmentationName)
-  segmentID = segmentationNode.GetSegmentation().GetSegmentIdBySegmentName(segmentName)
+  vector = vtk.vtkVector3d()
+  pt = [0, 0, 0]
+  resampledCurve = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsCurveNode", f"resampledCurve{label_id}")
+
+  for controlPoint in range(0, newPoints.GetNumberOfPoints()):
+    newPoints.GetPoint(controlPoint, pt)
+    vector[0] = pt[0]
+    vector[1] = pt[1]
+    vector[2] = pt[2]
+    resampledCurve.AddControlPoint(vector)
+  return resampledCurve
+
+def split_segment(segmentationNode):
+  # Initialize segment editor
+  segmentEditorWidget = slicer.qMRMLSegmentEditorWidget()
+  segmentEditorWidget.setMRMLScene(slicer.mrmlScene)
+  segmentEditorNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentEditorNode")
+  segmentEditorWidget.setMRMLSegmentEditorNode(segmentEditorNode)
+  # segm_node = slicer.util.getNode(f'{label_id}.label-segmentation')
+  segmentEditorWidget.setSegmentationNode(segmentationNode)
+  # volume_node = slicer.util.getNode(f'{label_id}.label')
+  # segmentEditorWidget.setMasterVolumeNode(volume_node)
+
+  # Split islands to segments
+  segmentEditorNode.SetSelectedSegmentID("Segment_1")
+  segmentEditorWidget.setActiveEffectByName("Islands")
+  effect = segmentEditorWidget.activeEffect()
+  effect.setParameter("MinimumSize", "500")
+  effect.setParameter("Operation", "SPLIT_ISLANDS_TO_SEGMENTS")
+  segmentEditorNode.SetOverwriteMode(slicer.vtkMRMLSegmentEditorNode.OverwriteNone)
+  segmentEditorNode.SetMaskMode(0)
+  effect.self().onApply()
+
+  # Cleanup
+  slicer.mrmlScene.RemoveNode(segmentEditorNode)
+
+def find_centerlines(segmentationNode, segmentID, vessel_id: int):
 
   # Preprocess the surface
   inputSurfacePolyData = extractLogic.polyDataFromNode(segmentationNode, segmentID)
@@ -23,15 +59,46 @@ def extract_centerlines_slicer(label_id: int):
   networkPolyData = extractLogic.extractNetwork(inputSurfacePolyData, endPointsMarkupsNode)
   startPointPosition = None
   endpointPositions = extractLogic.getEndPoints(networkPolyData, startPointPosition)
-  endPointsMarkupsNode.RemoveAllMarkups()
+  endPointsMarkupsNode.RemoveAllControlPoints()
   for position in endpointPositions:
     endPointsMarkupsNode.AddControlPoint(vtk.vtkVector3d(position))
 
   # Extract the centerline
-  centerlineCurveNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsCurveNode", "Centerline curve")
-  centerlinePolyData, voronoiDiagramPolyData = extractLogic.extractCenterline(inputSurfacePolyData, endPointsMarkupsNode)
+  centerlineCurveNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsCurveNode", f"Centerline curve {vessel_id}")
+  centerlinePolyData, voronoiDiagramPolyData = extractLogic.extractCenterline(inputSurfacePolyData,
+                                                                              endPointsMarkupsNode)
   centerlinePropertiesTableNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLTableNode", "Centerline properties")
   extractLogic.createCurveTreeFromCenterline(centerlinePolyData, centerlineCurveNode, centerlinePropertiesTableNode)
+
+def save_centerlines_to_file(label_id):
+  vessel_segements = slicer.util.getNodesByClass('vtkMRMLMarkupsCurveNode')
+
+  for i, segment in enumerate(vessel_segements):
+    # Resample curve
+    resampledCurve = resample_segment(segment)
+
+    # Save new curve to a file.
+    slicer.modules.markups.logic().ExportControlPointsToCSV(resampledCurve, f"C:\\Users\\20203226\\Documents\\CTA data\\Segments\\SegmentPoints_{label_id}_{i}.csv")
+
+def extract_centerlines_slicer(label_id: int):
+  # Load the CTA data
+  slicer.util.loadSegmentation(f"c:/Users/20203226/Documents/CTA data/1-200/{label_id}.label.nii.gz")
+
+  # Select the segmentation node
+  segmentationName = f'{label_id}.label.nii.gz'
+  segmentationNode = slicer.util.getNode(segmentationName)
+
+  # Split the segmentation in two
+  split_segment(segmentationNode)
+
+  # Find the centerlines for each vessel
+  for i, segmentName in enumerate(['Segment_1', 'Segment_1_2']):
+    segmentID = segmentationNode.GetSegmentation().GetSegmentIdBySegmentName(segmentName)
+    find_centerlines(segmentationNode, segmentID, i)
+
+  # Save centerlines to file
+  save_centerlines_to_file(label_id)
+
 
 if __name__ == "__main__":
   extract_centerlines_slicer(2)
