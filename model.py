d20779dfb9a7bcaa33ed190f44525a2b8bc66288
nasrelkad <98748702+nasrelkad@users.noreply.github.com>
2024-04-07 11:27:51 +0200

    Update model.py
    fixes

--- model.py
+++ model.py
@@ -1,131 +1,171 @@
 import torch
-from torch import nn, optim
-from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms
-from model import CARNet
+import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
-import matplotlib.pyplot as plt
-
-def convert_to_spherical(coordinates: np.array) -> (np.array, np.array):
-    """
-        Convert Cartesian coordinates to spherical coordinates.
-
-        Parameters:
-        - coordinates (np.array): An array containing Cartesian coordinates of points in three-dimensional space.
-
-        Returns:
-        - origin_tensor (np.array): The original point in Cartesian coordinates.
-        - spherical_coordinates (np.array): An array containing spherical coordinates (r, theta, phi) for each point.
-          - r (float): Radial distance from the origin.
-          - theta (float): Polar angle, in radians, measured from the positive z-axis.
-          - phi (float): Azimuthal angle, in radians, measured from the positive x-axis in the xy-plane.
-    """
-    spherical_coordinates = []
-    origin_tensor = coordinates[0,:]
-
-    for i in range(coordinates.shape[0]-1):
-        [x,y,z] = coordinates[i+1,:] - coordinates[i,:]
-        r = np.sqrt(x**2 + y**2 + z**2)
-        theta = np.arccos(z/r)
-        phi = np.sign(y)*np.arccos(x/(np.sqrt(x**2+y**2)))
-        spherical_coordinates.append([r, theta, phi])
-
-    return origin_tensor, np.array(spherical_coordinates)
+import sperical_coordinates
 
 
-# Load 3D data
-data_3d = np.genfromtxt('Segments_renamed/Segments_renamed/SegmentPoints_0001_00.csv', delimiter=',', skip_header=1)
-coordinates_3d = data_3d[:, 1:4]  # Adjust based on your CSV's column layout
-
-# Load 2D data
-data_2d = np.genfromtxt('Segments_deformed_2/Segments_deformed_2/SegmentPoints_0001_00_def2D.csv', delimiter=',', skip_header=1)
-d_data = data_2d[:, :3]  # Assuming first two columns after header are x, y coordinates
-# Append a column of zeros to make it "3D" for processing
-coordinates_2d = np.hstack((d_data, np.zeros((d_data.shape[0], 1))))
-
-model = CARNet()
-
-origin_3d, spherical_3d = convert_to_spherical(coordinates_3d)
-origin_2d, spherical_2d = convert_to_spherical(coordinates_2d)
-
-x3d_origin = torch.tensor(origin_3d, dtype=torch.float).view(1, 3, 1)  # Now shape is [1, 3, 1]
-x2d_origin = torch.tensor(origin_2d, dtype=torch.float).view(1, 3, 1)  # Now shape is [1, 3, 1]
-
-# Adjusting x3d_shape and x2d_shape to have shape (1, N, 3)
-x3d_shape = torch.tensor(spherical_3d, dtype=torch.float).unsqueeze(0).transpose(1, 2)  # Now shape is [1, 349, 3]
-x2d_shape = torch.tensor(spherical_2d, dtype=torch.float).unsqueeze(0).transpose(1, 2)
-
-
-# Set the model to evaluation mode
-model.eval()
-
-# Pass the data through the model
-with torch.no_grad():
-    deformation_field = model(x3d_origin, x3d_shape, x2d_origin, x2d_shape)
-
-# Print the shape of the output to understand its dimensions
-print("Output shape:", deformation_field)
-
-def convert_back_with_deformation_field(origin_tensor: np.array, spherical_coordinates: np.array, deformation_field: np.array) -> np.array:
-    """
-    Convert spherical coordinates back to Cartesian coordinates.
-
-    Parameters:
-    - origin_tensor (np.array): The original point in Cartesian coordinates.
-    - spherical_coordinates (np.array): An array containing spherical coordinates (r, theta, phi) for each point.
-      - r (float): Radial distance from the origin.
-      - theta (float): Polar angle, in radians, measured from the positive z-axis.
-      - phi (float): Azimuthal angle, in radians, measured from the positive x-axis in the xy-plane.
-
-    Returns:
-    - cartesian_coordinates (np.array): An array containing Cartesian coordinates of points in three-dimensional space.
-    """
-    cartesian_coordinates = []
-    cartesian_coordinates.append(origin_tensor)
-
-    matrix = torch.ones((349, 349))
-    matrix = torch.triu(matrix)
-    for i in range(spherical_coordinates.shape[0]):
-        [r, theta, phi] = spherical_coordinates[i]
-        x = r * torch.sin(theta + deformation_field[0, 0]) * torch.sin(phi + deformation_field[0, 1])            
-        y = r * torch.sin(theta + deformation_field[0, 0]) * torch.cos(phi + deformation_field[0, 1])
-        z = r * torch.cos(theta + deformation_field[0, 0])
+class DualBranch(nn.Module):
+    def __init__(self):
+        super(DualBranch, self).__init__()
+        self.origin_branch = nn.Sequential(
+            nn.Conv1d(in_channels=3, out_channels=8, kernel_size=1),
+            nn.BatchNorm1d(8),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=8, out_channels=16, kernel_size=1),
+            nn.BatchNorm1d(16),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=16, out_channels=32, kernel_size=1),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=1),     
+        )
         
-        x = torch.add(torch.matmul(x, matrix), cartesian_coordinates[i][0][0])
-        y = torch.add(torch.matmul(y, matrix), cartesian_coordinates[i][0][1])
-        z = torch.add(torch.matmul(z, matrix), cartesian_coordinates[i][0][2])
+        self.shape_branch = nn.Sequential(
+            nn.Conv1d(in_channels=3, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            
+
+        )
+        self.bn_relu = nn.Sequential(
+            nn.BatchNorm1d(64),
+            nn.ReLU()
+        )
+
+    def forward(self, origin, shape):
+        origin_feat = self.origin_branch(origin)
+        shape_feat = self.shape_branch(shape)
+
+        return self.bn_relu(torch.add(origin_feat, shape_feat))
         
 
-        cartesian_coordinates.append(torch.stack((x,y,z)))
 
-    return cartesian_coordinates[1]
+class Downsample(nn.Module):
+    def __init__(self, in_channels, out):
+        super(Downsample, self).__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv1d(in_channels, out_channels=64, kernel_size=1),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=64, out_channels=64, kernel_size=3, padding=1, stride=2),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=64, out_channels=out, kernel_size=1)
+        )
 
-reconstructed_coordinates = convert_back_with_deformation_field(x3d_origin, x3d_shape, deformation_field)
+        self.bn_relu_add = nn.Sequential(
+            nn.BatchNorm1d(out),
+            nn.ReLU()
+        )
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+        self.conv2 = nn.Conv1d(in_channels=in_channels, out_channels=out, kernel_size=3, padding=1, stride=2)
 
-# Extract x, y, z coordinates from the input array
-                              
-x = coordinates_3d[:, 0]
-y = coordinates_3d[:, 1]
-z = coordinates_3d[:, 2]
+    def forward(self, x):
+        return self.bn_relu_add(torch.add(self.conv_layers(x), self.conv2(x)))
+    
+class grey(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(grey, self).__init__()
 
-x_r = reconstructed_coordinates[0, :]
-y_r = reconstructed_coordinates[1, :]
-z_r = reconstructed_coordinates[2, :]
+        self.conv = nn.Sequential(
+            nn.Conv1d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm1d(out_channels),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+class orange(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(orange, self).__init__()
+        self.upconv = nn.ConvTranspose1d(in_channels, out_channels, kernel_size=3, padding =1, stride =2, output_padding=1)
+        
+    def forward(self, x, prev_info):
+        upsampled_x = self.upconv(x)
+        # If upsampled_x is larger, crop it to match prev_info's size
+        target_length = prev_info.size()[2]
+        current_length = upsampled_x.size()[2]
+        if current_length > target_length:
+            # Calculate cropping
+            crop_left = (current_length - target_length) // 2
+            crop_right = current_length - crop_left - target_length
+            # Apply cropping
+            upsampled_x = upsampled_x[:, :, crop_left:current_length-crop_right]
+        return torch.cat((upsampled_x, prev_info), dim=1)
 
 
-x_2D = d_data[:, 0]
-y_2D = d_data[:, 1]
+class CARNet(nn.Module):
+    def __init__(self):
+        super(CARNet, self).__init__()
+        self.dual_branch_3d = DualBranch()
+        self.dual_branch_2d = DualBranch()
+        self.unet_backbone = UNet()
 
-# Plot the line connecting the points
-ax.plot(x, y, z,)
-ax.plot(x_r, y_r, z_r)
-ax.plot(x_2D, y_2D)
+    def forward(self, x3d_origin, x3d_shape, x2d_origin, x2d_shape):
+        
+        x3d = self.dual_branch_3d(x3d_origin, x3d_shape)
+        x2d = self.dual_branch_2d(x2d_origin, x2d_shape)
+        
+        
+        x = torch.cat((x3d, x2d), dim=1)
+        
+        deformation_field = self.unet_backbone(x)
+        
+        return deformation_field
 
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.legend(['Original centerline segment', 'Deformed centerline segment',  '2D loaded from csv file'])
-plt.show()
+class UNet(nn.Module):
+    def __init__(self):
+        super(UNet, self).__init__()
+        
+        self.down1 = grey(128, 64)
+        self.down2 = Downsample(64, 128)
+        self.down3 = Downsample(128, 256)  
+        self.down4 = Downsample(256, 512)  
+        
+
+        self.bridge = grey(512, 512)
+        
+        
+        self.up1 = orange(512, 256)  
+        self.up2 = grey(512, 256)
+        self.up3 = orange(256, 128)  
+        self.up4 = grey(256, 128)
+        self.up5 = orange(128, 64)  
+        self.up6 = grey(128, 64)
+
+        
+        self.output_layer = nn.Conv1d(64, 2, kernel_size=1)
+
+    def forward(self, x):
+        
+        conn1 = self.down1(x)
+        skip_conn2 = self.down2(conn1)
+        skip_conn3 = self.down3(skip_conn2)
+        skip_conn4 = self.down4(skip_conn3)
+        
+        x = self.bridge(skip_conn4)
+        
+        
+        x = self.up1(x, skip_conn3)
+        x = self.up2(x)
+        x = self.up3(x, skip_conn2)
+        x = self.up4(x)
+        x = self.up5(x, conn1)
+        x = self.up6(x)
+        
+        
+        deformation_field = self.output_layer(x)
+        return deformation_field.numpy()
+    
+
+    
+    
