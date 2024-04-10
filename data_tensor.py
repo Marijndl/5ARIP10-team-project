e1a438dbe5020ad2ef73492f3593cc3c33512800
Marijn <51094560+Marijndl@users.noreply.github.com>
2024-04-10 13:41:16 +0200

    Remove nans from data

--- data_tensor.py
+++ data_tensor.py
@@ -6,7 +6,7 @@ from model import CARNet
 import numpy as np
 import os
 from spherical_coordinates import *
-from torchsummary import summary
+
 
 #Load data
 directory_2D = "D:\\CTA data\\Segments_deformed_2\\"
@@ -21,10 +21,12 @@ sha_3D = torch.tensor([])
 
 lengthhh = len(file_names_2D)
 
+total_nans = 0
+
 for idx in range(lengthhh):
     print(f"{idx}/{lengthhh}")
-    data_2d = np.genfromtxt(os.path.join(directory_2D, file_names_2D[0]), delimiter=",")
-    data_3d = np.genfromtxt(os.path.join(directory_3D, file_names_3D[0]), delimiter=",")[1:, 1:4]
+    data_2d = np.genfromtxt(os.path.join(directory_2D, file_names_2D[idx]), delimiter=",")
+    data_3d = np.genfromtxt(os.path.join(directory_3D, file_names_3D[idx]), delimiter=",")[1:, 1:4]
 
     #Add row of zeros
     data_2d = np.hstack((data_2d, np.zeros((data_2d.shape[0],1))))
@@ -38,19 +40,23 @@ for idx in range(lengthhh):
     origin_2D, shape_2D = convert_to_spherical(data_2d)
     origin_3D, shape_3D = convert_to_spherical(data_3d)
 
-    origin_2D = torch.reshape(origin_2D, (1, 3,1)).float()
-    origin_3D = torch.reshape(origin_3D, (1, 3,1)).float()
+    origin_2D = torch.reshape(origin_2D, (1,3,1)).float()
+    origin_3D = torch.reshape(origin_3D, (1,3,1)).float()
 
-    shape_2D = torch.reshape(shape_2D, (1, 3, 349)).float()
-    shape_3D = torch.reshape(shape_3D, (1, 3, 349)).float()
+    shape_2D = torch.unsqueeze(torch.transpose(shape_2D, 0, 1), dim=0).float()
+    shape_3D = torch.unsqueeze(torch.transpose(shape_3D, 0, 1), dim=0).float()
 
-    org_2D = torch.cat((org_2D, origin_2D), dim=0)
-    org_3D = torch.cat((org_3D, origin_3D), dim=0)
-    sha_2D = torch.cat((sha_2D, shape_2D), dim=0)
-    sha_3D = torch.cat((sha_3D, shape_3D), dim=0)
+    if torch.sum(torch.isnan(shape_2D)) >= 1 or torch.sum(torch.isnan(shape_3D)) >= 1:
+        print(f"Found nan in {idx}")
+        total_nans += torch.sum(torch.isnan(shape_2D)) + torch.sum(torch.isnan(shape_3D))
+    else:
+        org_2D = torch.cat((org_2D, origin_2D), dim=0)
+        org_3D = torch.cat((org_3D, origin_3D), dim=0)
+        sha_2D = torch.cat((sha_2D, shape_2D), dim=0)
+        sha_3D = torch.cat((sha_3D, shape_3D), dim=0)
 
 torch.save(org_2D, 'D:\\CTA data\\origin_2D.pt')
 torch.save(org_3D, 'D:\\CTA data\\origin_3D.pt')
 torch.save(sha_2D, 'D:\\CTA data\\shape_2D.pt')
 torch.save(sha_3D, 'D:\\CTA data\\shape_3D.pt')
-print("Done")
+print(f"Done, total nans: {total_nans}")
