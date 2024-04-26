99765856bca89f67b3ab887b4775aef6501a6591
Marijn <51094560+Marijndl@users.noreply.github.com>
2024-04-26 10:42:25 +0200

    Show result

---
+++ plot_interpolation.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import torch
+mpl.use('Qt5Agg')
+from scipy import interpolate
+import re
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+base_dir = "D:\\CTA data\\Segments original\\"
+interp_dir = "D:\\CTA data\\Segments bspline\\"
+i = 0
+
+data = np.genfromtxt(f"D:\\CTA data\\Segments original\\SegmentPoints_0001_{str(i).zfill(2)}.csv", delimiter=",")[1:,1:4]
+x = data[:, 0]
+y = data[:, 1]
+z = data[:, 2]
+ax.plot(x, y, z, '--g', label='Original Data')
+
+data = np.genfromtxt(f"D:\\CTA data\\Segments bspline\\SegmentPoints_0001_{str(i).zfill(2)}.csv", delimiter=",")[1:, :]
+x_new = data[:, 0]
+y_new = data[:, 1]
+z_new = data[:, 2]
+ax.plot(x_new, y_new, z_new, '-c', label='B-spline Interpolation')
+
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')
+ax.set_zlabel('Z-axis')
+plt.title('B-spline Interpolation in 3D')
+plt.legend(loc='best')
+plt.show()
