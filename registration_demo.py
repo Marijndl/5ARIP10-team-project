7d2e616ff4dabc5db472930e65761158790b63cf
Marijn <51094560+Marijndl@users.noreply.github.com>
2024-05-09 14:29:21 +0200

    Add 3d line to demo

--- registration_demo.py
+++ registration_demo.py
@@ -36,12 +36,13 @@ spherical_3D_deformed = sample['shape_3D'].clone().to('cuda')
 spherical_3D_deformed[:, 1:, :] = torch.add(spherical_3D_deformed[:, 1:, :], deformation_field)
 
 # Convert back to cartesian domain
-deformed_cart = loss.cartesian_tensor(sample['origin_3D'].to('cuda'), spherical_3D_deformed)
-original_cart = loss.cartesian_tensor(sample['origin_2D'].to('cuda'), sample['shape_2D'].to('cuda'))
+deformed_cart_3D = loss.cartesian_tensor(sample['origin_3D'].to('cuda'), spherical_3D_deformed)
+original_cart_3D = loss.cartesian_tensor(sample['origin_3D'].to('cuda'), sample['shape_3D'].to('cuda'))
+original_cart_2D = loss.cartesian_tensor(sample['origin_2D'].to('cuda'), sample['shape_2D'].to('cuda'))
 
-original = original_cart.clone().detach().cpu().numpy()
-deformed = deformed_cart.clone().detach().cpu().numpy()
-
+deformed_3D = deformed_cart_3D.clone().detach().cpu().numpy()
+original_3D = original_cart_3D.clone().detach().cpu().numpy()
+original_2D = original_cart_2D.clone().detach().cpu().numpy()
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -49,22 +50,28 @@ ax = fig.add_subplot(111, projection='3d')
 idx = 0
 
 # Extract x, y, z coordinates from the input array
-org_x = original[idx, 0, :]
-org_y = original[idx, 1, :]
-# org_z = original[idx, 2, :]
-org_z = np.zeros(original[idx, 2, :].shape) # To prevent the small non-zero values to influence the plot
+org_x = original_2D[idx, 0, :]
+org_y = original_2D[idx, 1, :]
+# org_z = original_2D[idx, 2, :]
+org_z = np.zeros(original_2D[idx, 2, :].shape) # To prevent the small non-zero values to influence the plot
 
-def_x = deformed[idx, 0, :]
-def_y = deformed[idx, 1, :]
-# def_z = deformed[idx, 2, :]
-def_z = np.zeros(original[idx, 2, :].shape)
+def_x = deformed_3D[idx, 0, :]
+def_y = deformed_3D[idx, 1, :]
+# def_z = deformed_3D[idx, 2, :]
+def_z = np.zeros(original_2D[idx, 2, :].shape)
 
-# Plot the line connecting the points
+org_x_3 = original_3D[idx, 0, :]
+org_y_3 = original_3D[idx, 1, :]
+org_z_3 = original_3D[idx, 2, :]
+
+
+# Plot the lines connecting the points
 ax.plot(org_x, org_y, org_z)
 ax.plot(def_x, def_y, def_z)
+ax.plot(org_x_3, org_y_3, org_z_3)
 
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
-ax.legend(['Original centerline segment', 'Deformed centerline segment'])
+ax.legend(['Original 2D centerline segment', 'Deformed 3D centerline segment', 'Original 3D centerline segment'])
 plt.show()
