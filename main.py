5ffde482373ae5ed1161ad8c45b8029041e2c09e
nasrelkad <98748702+nasrelkad@users.noreply.github.com>
2024-05-29 18:45:33 +0200

    Update main.py
    latest version nasr used

--- main.py
+++ main.py
@@ -1,131 +1,161 @@
 import torch
 from torch import nn, optim
-from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms
 from model import CARNet
-import numpy as np
+from spherical_coordinates import *
+from tqdm import tqdm
+from data_loaders import *
+from torch.utils.data import DataLoader, random_split
 import matplotlib.pyplot as plt
 
-def convert_to_spherical(coordinates: np.array) -> (np.array, np.array):
-    """
-        Convert Cartesian coordinates to spherical coordinates.
+class mPD_loss_2(nn.Module):
+    def __init__(self):
+        super().__init__()
 
-        Parameters:
-        - coordinates (np.array): An array containing Cartesian coordinates of points in three-dimensional space.
+    def cartesian_tensor(self, origin, spherical):
+        r = spherical[:, 0, :].clone().requires_grad_(True)
+        theta = spherical[:, 1, :].clone().requires_grad_(True)
+        phi = spherical[:, 2, :].clone().requires_grad_(True)
 
-        Returns:
-        - origin_tensor (np.array): The original point in Cartesian coordinates.
-        - spherical_coordinates (np.array): An array containing spherical coordinates (r, theta, phi) for each point.
-          - r (float): Radial distance from the origin.
-          - theta (float): Polar angle, in radians, measured from the positive z-axis.
-          - phi (float): Azimuthal angle, in radians, measured from the positive x-axis in the xy-plane.
-    """
-    spherical_coordinates = []
-    origin_tensor = coordinates[0,:]
+        x = r * torch.sin(theta) * torch.cos(phi)
+        y = r * torch.sin(theta) * torch.sin(phi)
+        z = r * torch.cos(theta)
 
-    for i in range(coordinates.shape[0]-1):
-        [x,y,z] = coordinates[i+1,:] - coordinates[i,:]
-        r = np.sqrt(x**2 + y**2 + z**2)
-        theta = np.arccos(z/r)
-        phi = np.sign(y)*np.arccos(x/(np.sqrt(x**2+y**2)))
-        spherical_coordinates.append([r, theta, phi])
+        shape_cartesian = torch.cat((x.unsqueeze(dim=1), y.unsqueeze(dim=1), z.unsqueeze(dim=1)), dim=1).requires_grad_(True)
+        full = torch.cat((origin.clone(), shape_cartesian), dim=2).requires_grad_(True)
 
-    return origin_tensor, np.array(spherical_coordinates)
+        cartesian = torch.matmul(full, torch.triu(torch.ones((x.shape[0], full.shape[2], full.shape[2]))).to(device)).requires_grad_(True)
+        return cartesian
 
-
-# Load 3D data
-data_3d = np.genfromtxt('Segments_renamed/Segments_renamed/SegmentPoints_0001_00.csv', delimiter=',', skip_header=1)
-coordinates_3d = data_3d[:, 1:4]  # Adjust based on your CSV's column layout
-
-# Load 2D data
-data_2d = np.genfromtxt('Segments_deformed_2/Segments_deformed_2/SegmentPoints_0001_00_def2D.csv', delimiter=',', skip_header=1)
-d_data = data_2d[:, :3]  # Assuming first two columns after header are x, y coordinates
-# Append a column of zeros to make it "3D" for processing
-coordinates_2d = np.hstack((d_data, np.zeros((d_data.shape[0], 1))))
-
-model = CARNet()
-
-origin_3d, spherical_3d = convert_to_spherical(coordinates_3d)
-origin_2d, spherical_2d = convert_to_spherical(coordinates_2d)
-
-x3d_origin = torch.tensor(origin_3d, dtype=torch.float).view(1, 3, 1)  # Now shape is [1, 3, 1]
-x2d_origin = torch.tensor(origin_2d, dtype=torch.float).view(1, 3, 1)  # Now shape is [1, 3, 1]
-
-# Adjusting x3d_shape and x2d_shape to have shape (1, N, 3)
-x3d_shape = torch.tensor(spherical_3d, dtype=torch.float).unsqueeze(0).transpose(1, 2)  # Now shape is [1, 349, 3]
-x2d_shape = torch.tensor(spherical_2d, dtype=torch.float).unsqueeze(0).transpose(1, 2)
-
-
-# Set the model to evaluation mode
-model.eval()
-
-# Pass the data through the model
-with torch.no_grad():
-    deformation_field = model(x3d_origin, x3d_shape, x2d_origin, x2d_shape)
-
-# Print the shape of the output to understand its dimensions
-print("Output shape:", deformation_field)
-
-def convert_back_with_deformation_field(origin_tensor: np.array, spherical_coordinates: np.array, deformation_field: np.array) -> np.array:
-    """
-    Convert spherical coordinates back to Cartesian coordinates.
-
-    Parameters:
-    - origin_tensor (np.array): The original point in Cartesian coordinates.
-    - spherical_coordinates (np.array): An array containing spherical coordinates (r, theta, phi) for each point.
-      - r (float): Radial distance from the origin.
-      - theta (float): Polar angle, in radians, measured from the positive z-axis.
-      - phi (float): Azimuthal angle, in radians, measured from the positive x-axis in the xy-plane.
-
-    Returns:
-    - cartesian_coordinates (np.array): An array containing Cartesian coordinates of points in three-dimensional space.
-    """
-    cartesian_coordinates = []
-    cartesian_coordinates.append(origin_tensor)
-
-    matrix = torch.ones((349, 349))
-    matrix = torch.triu(matrix)
-    for i in range(spherical_coordinates.shape[0]):
-        [r, theta, phi] = spherical_coordinates[i]
-        x = r * torch.sin(theta + deformation_field[0, 0]) * torch.sin(phi + deformation_field[0, 1])            
-        y = r * torch.sin(theta + deformation_field[0, 0]) * torch.cos(phi + deformation_field[0, 1])
-        z = r * torch.cos(theta + deformation_field[0, 0])
+    def smoothness_loss(self, deformation_field):
+        def gradient(tensor):
+            grad_y = tensor[:, :, 1:] - tensor[:, :, :-1]
+            grad_x = tensor[:, 1:, :] - tensor[:, :-1, :]
+            return grad_y, grad_x
         
-        x = torch.add(torch.matmul(x, matrix), cartesian_coordinates[i][0][0])
-        y = torch.add(torch.matmul(y, matrix), cartesian_coordinates[i][0][1])
-        z = torch.add(torch.matmul(z, matrix), cartesian_coordinates[i][0][2])
+        grad_y, grad_x = gradient(deformation_field)
         
+        smooth_loss = torch.sum(grad_y**2) + torch.sum(grad_x**2)
+        return smooth_loss
 
-        cartesian_coordinates.append(torch.stack((x,y,z)))
+    def forward(self, origin_3D, spherical_3D, origin_2D, spherical_2D, deformation_field):
+        spherical_3D_deformed = spherical_3D.clone()
+        spherical_3D_deformed[:, 1:, :] = torch.add(spherical_3D_deformed[:, 1:, :], deformation_field)
 
-    return cartesian_coordinates[1]
+        deformed_cart = self.cartesian_tensor(origin_3D, spherical_3D_deformed)
+        original_cart = self.cartesian_tensor(origin_2D, spherical_2D)
 
-reconstructed_coordinates = convert_back_with_deformation_field(x3d_origin, x3d_shape, deformation_field)
+        loss = torch.sum(torch.mean(torch.sum(torch.abs(deformed_cart - original_cart), dim=1), dim=1))
+        smooth_loss = self.smoothness_loss(deformation_field)
+        
+        total_loss = loss + smooth_loss
+        return total_loss
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+torch.autograd.set_detect_anomaly(True)
+torch.cuda.empty_cache()
 
-# Extract x, y, z coordinates from the input array
-                              
-x = coordinates_3d[:, 0]
-y = coordinates_3d[:, 1]
-z = coordinates_3d[:, 2]
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-x_r = reconstructed_coordinates[0, :]
-y_r = reconstructed_coordinates[1, :]
-z_r = reconstructed_coordinates[2, :]
+def weights_init(m):
+    if isinstance(m, nn.Conv1d):
+        nn.init.normal_(m.weight.data, mean=0.0, std=1)
+    elif isinstance(m, nn.ConvTranspose1d):
+        nn.init.normal_(m.weight.data, mean=0.0, std=1)
 
+model = CARNet().to(device)
+model.apply(weights_init)
+criterion = mPD_loss_2()
+optimizer = optim.Adam(model.parameters(), lr=0.1, weight_decay=1e-4)
 
-x_2D = d_data[:, 0]
-y_2D = d_data[:, 1]
+total_params = sum(p.numel() for p in model.parameters())
+print(f"Number of parameters: {total_params}")
 
-# Plot the line connecting the points
-ax.plot(x, y, z,)
-ax.plot(x_r, y_r, z_r)
-ax.plot(x_2D, y_2D)
+def train_model(model, criterion, optimizer, train_loader, val_loader, num_epochs=186):
+    train_losses = []
+    val_losses = []
 
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.legend(['Original centerline segment', 'Deformed centerline segment',  '2D loaded from csv file'])
-plt.show()
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+
+        loop = tqdm(enumerate(train_loader), total=len(train_loader), leave=True)
+        for batch_idx, input in loop:
+            optimizer.zero_grad()
+
+            input['origin_3D'] = input['origin_3D'].to(device).requires_grad_(True)
+            input['shape_3D'] = input['shape_3D'].to(device).requires_grad_(True)
+            input['origin_2D'] = input['origin_2D'].to(device).requires_grad_(True)
+            input['shape_2D'] = input['shape_2D'].to(device).requires_grad_(True)
+
+            outputs = model(input['origin_3D'], input['shape_3D'], input['origin_2D'], input['shape_2D'])
+
+            loss = criterion(input['origin_3D'], input['shape_3D'], input['origin_2D'], input['shape_2D'], outputs)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item() * input['origin_3D'].shape[0]
+            loop.set_description(f"Epoch [{epoch+1}/{num_epochs}]")
+            loop.set_postfix(loss=loss.item())
+
+        epoch_train_loss = running_loss / len(train_loader.dataset)
+        train_losses.append(epoch_train_loss)
+
+        model.eval()
+        val_loss = 0.0
+        with torch.no_grad():
+            for input in val_loader:
+                input['origin_3D'] = input['origin_3D'].to(device)
+                input['shape_3D'] = input['shape_3D'].to(device)
+                input['origin_2D'] = input['origin_2D'].to(device)
+                input['shape_2D'] = input['shape_2D'].to(device)
+                outputs = model(input['origin_3D'], input['shape_3D'], input['origin_2D'], input['shape_2D'])
+                loss = criterion(input['origin_3D'], input['shape_3D'], input['origin_2D'], input['shape_2D'], outputs)
+                val_loss += loss.item() * input['origin_3D'].shape[0]
+
+        epoch_val_loss = val_loss / len(val_loader.dataset)
+        val_losses.append(epoch_val_loss)
+        print(f'Epoch {epoch+1}, Train Loss: {epoch_train_loss}, Validation Loss: {epoch_val_loss}')
+
+    return model, train_losses, val_losses
+
+def test_model(model, criterion, test_loader):
+    model.eval()
+    test_loss = 0.0
+    with torch.no_grad():
+        for input in test_loader:
+            input['origin_3D'] = input['origin_3D'].to(device)
+            input['shape_3D'] = input['shape_3D'].to(device)
+            input['origin_2D'] = input['origin_2D'].to(device)
+            input['shape_2D'] = input['shape_2D'].to(device)
+            outputs = model(input['origin_3D'], input['shape_3D'], input['origin_2D'], input['shape_2D'])
+            loss = criterion(input['origin_3D'], input['shape_3D'], input['origin_2D'], input['shape_2D'], outputs)
+            test_loss += loss.item() * input['origin_3D'].shape[0]
+    
+    test_loss = test_loss / len(test_loader.dataset)
+    print(f'Test Loss: {test_loss}')
+    return test_loss
+
+if __name__ == "__main__":
+    dataset = CenterlineDatasetSpherical(base_dir="D:\\CTA data\\")
+    train_size = int(0.7 * len(dataset))
+    val_size = int(0.15 * len(dataset))
+    test_size = len(dataset) - train_size - val_size
+    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=512, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=512, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=512, shuffle=False)
+
+    trained_model, train_losses, val_losses = train_model(model, criterion, optimizer, train_loader, val_loader, num_epochs=100)
+
+    torch.save(trained_model.state_dict(), "D:\\CTA data\\models\\CAR-Net-256-23.pth")
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(train_losses, label='Train Loss')
+    plt.plot(val_losses, label='Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.title('Training and Validation Loss')
+    plt.show()
+
+    test_loss = test_model(trained_model, criterion, test_loader)
